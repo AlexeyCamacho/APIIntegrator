c00feab5096f6e4ff81e758fd6e729966163b0b2
Алексей Байков <aleksei.00004@mail.ru>
2023-02-25 01:41:11 +0400

    Трейт для управления правами и ролями

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use App\Models\Permission;
 
 class AdminController extends Controller
 {
-    //
+    public function view(Request $request)
+    {
+        $user = $request->user();
+        dd($user->giveRoleTo(null, 'admin'));
+    }
 }
---
+++ app/Traits/HasRolesAndPermissions.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Role;
+use App\Models\Permission;
+
+trait HasRolesAndPermissions
+{
+    public function roles($company = null)
+    {
+        return $this->belongsToMany(Role::class,'users_roles_companies')
+            ->withPivot('company_id')->where('company_id', $company)->withTimestamps();
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class,'users_permissions')->withTimestamps();
+    }
+
+    public function hasRole($company = null, ... $roles) {
+        foreach ($roles as $role) {
+            if ($this->roles($company)->get()->contains('slug', $role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function hasPermission($permission, $company = null)
+    {
+        return (bool) $this->permissions($company)->where('slug', $permission)->count();
+    }
+
+    public function hasPermissionTo($permission, $company = null)
+    {
+        return $this->hasPermissionThroughRole($permission, $company) || $this->hasPermission($permission->slug, $company);
+    }
+
+    public function hasPermissionThroughRole($permission, $company = null)
+    {
+        foreach ($permission->roles as $role){
+            if($this->roles($company)->get()->contains($role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getAllPermissions(array $permissions)
+    {
+        return Permission::whereIn('slug',$permissions)->get();
+    }
+
+    public function givePermissionsTo(... $permissions)
+    {
+        $permissions = $this->getAllPermissions($permissions);
+        if($permissions === null) {
+            return $this;
+        }
+        $this->permissions()->saveMany($permissions);
+        return $this;
+    }
+
+    public function deletePermissions(... $permissions )
+    {
+        $permissions = $this->getAllPermissions($permissions);
+        $this->permissions()->detach($permissions);
+        return $this;
+    }
+
+    public function refreshPermissions(... $permissions )
+    {
+        $this->permissions()->detach();
+        return $this->givePermissionsTo($permissions);
+    }
+
+    public function getAllRoles($role, $global = true)
+    {
+        return Role::where('slug',$role)->where('global', $global)->get()->first();
+    }
+
+    public function giveRoleTo($company, $role)
+    {
+        $role = $this->getAllRoles($role, !$company);
+        if ($role === null || $this->hasRole($company, $role->slug)) {
+            return $this;
+        }
+        $this->roles()->save($role, ['company_id' => $company]);
+        return $this;
+    }
+}
