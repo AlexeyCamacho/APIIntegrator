46b8ed27bbe27b57737448152e88a82aff91f33b
Алексей <Aleksei.00004@mail.ru>
2023-03-22 20:59:19 +0400

    Рефакторинг RoleController. Просмотр пользователей.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function get(Request $request)
+    {
+        if (!$request->user()->hasPermissionTo('view-user')) {
+            return Inertia::render('Errors/Error403');
+        }
+
+        return Inertia::render('Admin/Users',[
+            'users' => User::with('roles')->get(),
+        ]);
+    }
+}
