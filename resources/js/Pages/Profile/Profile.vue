58282be346373088f34d500b86bdb317e343f0ff
Алексей <Aleksei.00004@mail.ru>
2023-04-01 02:55:30 +0400

    Страница профиля

--- resources/js/Pages/Profile/Profile.vue
+++ resources/js/Pages/Profile/Profile.vue
@@ -1,17 +1,12 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-import {computed} from "vue";
 
 const props = defineProps({
     user: Object,
     companies: Object,
 })
 
-const regDate = computed(() => {
-    var d = new Date(props.user.created_at);
-    return d.getDate() + '.' + d.getMonth() + '.' + d.getFullYear();
-});
 
 </script>
 
@@ -27,9 +22,34 @@ const regDate = computed(() => {
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="flex flex-row border-green-300 border-t-2 bg-gray-200">
-                        <div class="basis-1/4">1</div>
-                        <div>1</div>
+                    <div class="flex flex-row gap-4">
+                        <div class="flex flex-col basis-1/4 gap-4">
+                            <div class="bg-slate-200 rounded p-2 shadow drop-shadow">
+                                <img
+                                    src="https://sun9-56.userapi.com/impg/sJB2W2dkGHiBhdYtLGwKuxi73t90-IZhjc4B3w/C_5On-8L24Q.jpg?size=1200x1600&quality=95&sign=9959112d5d3d4d2ccc2a1aa2622bea7a&type=album"
+                                    class="w-full aspect-square object-cover rounded shadow-lg mx-auto"
+                                    alt="Avatar" />
+                                <div class="my-2">
+                                    <div class="text-xl">{{ props.user.name }}</div>
+                                    <div class="text-sm">{{ props.user.email }}</div>
+                                </div>
+                            </div>
+                            <div class="bg-slate-200 rounded p-2 shadow drop-shadow">
+                                <div class="my-2">
+                                    <div>Статус:</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col grow gap-4">
+                            <div class="bg-slate-200 rounded p-2 shadow drop-shadow">
+                                <div class="text-xl">Компании</div>
+                                <div v-for="(item, key) in props.companies" class="my-3 flex flex-row justify-between
+                                    border-2 border-blue-400 border-t-blue-700 border-l-blue-700 p-2 rounded-lg ">
+                                    <div>{{ item.name }}</div>
+                                </div>
+                            </div>
+                            <div class="bg-slate-200 rounded p-2 shadow drop-shadow">Устройства</div>
+                        </div>
                     </div>
                 </div>
             </div>
